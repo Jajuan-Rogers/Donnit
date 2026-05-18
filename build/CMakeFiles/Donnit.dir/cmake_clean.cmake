02d4c76940673cc857e110beb70ccbeedbace4b2
Ben <Bjornhephaestuts@proton.me>
2026-05-18 11:56:18 -0700

    wip

---
+++ build/CMakeFiles/Donnit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Donnit.dir/link.d"
+  "CMakeFiles/Donnit.dir/src/main.cpp.o"
+  "CMakeFiles/Donnit.dir/src/main.cpp.o.d"
+  "Donnit"
+  "Donnit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Donnit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <ncpp/NotCurses.hh>
+#include <ncpp/ncpp.hh>
+#include <fmt/format.h>
+
+
+int main() {
+  auto a = fmt::format("hello");
+  return 0;
+}
